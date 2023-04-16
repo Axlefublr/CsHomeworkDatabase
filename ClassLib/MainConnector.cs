fff12802c0dd2d6931a18cd521bfb1c78a2bac09
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-04-16 18:20:55 +0800

    add GetConnection

--- ClassLib/MainConnector.cs
+++ ClassLib/MainConnector.cs
@@ -4,6 +4,7 @@ namespace ClassLib;
 
 public class MainConnector
 {
+    SqlConnection connection;
     public async Task<bool> ConnectAsync()
     {
         bool result;
@@ -25,6 +26,18 @@ public class MainConnector
         if (connection.State == ConnectionState.Open)
         {
             await connection.CloseAsync();
+        }
+    }
+
+    public SqlConnection GetConnection()
+    {
+        if (connection.State == ConnectionState.Open)
+        {
+            return connection;
+        }
+        else
+        {
+            throw new Exception("Подключение уже закрыто!");
         }
     }
 }
