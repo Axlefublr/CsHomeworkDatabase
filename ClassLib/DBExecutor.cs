c3c8e667c532611b6714519425e143dfe0183011
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-04-16 18:32:45 +0800

    add DBExecutor

---
+++ ClassLib/DBExecutor.cs
@@ -0,0 +1,11 @@
+namespace ClassLib;
+
+public class DbExecutor
+{
+    private MainConnector connector;
+    public DbExecutor(MainConnector connector)
+    {
+        this.connector = connector;
+    }
+
+}
