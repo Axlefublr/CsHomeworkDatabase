2bc08095bf413792069a6f9dd412d8c0f0ee7fb9
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-04-16 18:58:07 +0800

    add data getting on bootup

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,21 +1,34 @@
-﻿using ClassLib;
+﻿using System.Data;
+using ClassLib;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        var connector = new MainConnector();
+        MainConnector connector = new();
 
         var result = connector.ConnectAsync();
 
-        if (result.Result)
-        {
-            Console.WriteLine("Подключено успешно!");
-        }
-        else
+        DataTable data = new();
+
+        if (!result.Result)
         {
             Console.WriteLine("Ошибка подключения!");
+            Console.ReadKey();
         }
+
+        Console.WriteLine("Подключено успешно!");
+
+        DbExecutor db = new(connector);
+
+        string tablename = "NetworkUser";
+
+        Console.WriteLine("Получаем данные таблицы " + tablename);
+
+        data = db.SelectAll(tablename);
+
+        Console.WriteLine("Отключаем БД!");
+        connector.DisconnectAsync();
 
         Console.ReadKey();
     }
