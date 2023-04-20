291125da84386808d367fd07690ab43729d42907
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-04-20 22:51:40 +0800

    add Manager

--- ADONETConsole/Program.cs
+++ ADONETConsole/Program.cs
@@ -7,47 +7,10 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        MainConnector connector = new();
-
-        var result = connector.ConnectAsync();
-
-        DataTable data = new();
-
-        if (!result.Result)
-        {
-            Console.WriteLine("Ошибка подключения!");
-            Console.ReadKey();
-        }
-
-        Console.WriteLine("Подключено успешно!");
-
-        DbExecutor db = new(connector);
-
-        string tablename = "NetworkUser";
-
-        Console.WriteLine("Получаем данные таблицы " + tablename);
-
-        data = db.SelectAll(tablename);
-
-        foreach (DataColumn column in data.Columns)
-        {
-            Console.WriteLine($"{column.ColumnName}\t");
-        }
-        Console.WriteLine();
-
-        foreach (DataRow row in data.Rows)
-        {
-            var cells = row.ItemArray;
-            foreach (var cell in cells)
-            {
-                Console.Write($"{cell}\t");
-            }
-            Console.WriteLine();
-        }
-
-        Console.WriteLine("Отключаем БД!");
-        connector.DisconnectAsync();
-
+        Manager manager = new();
+        manager.Connect();
+        manager.ShowData();
+        manager.Disconnect();
         Console.ReadKey();
     }
 }
